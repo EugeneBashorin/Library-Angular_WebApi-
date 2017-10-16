5f9801d439b6e74157a2c6df82e79e73c62b532d
Anuitex-17 <Anuitex-17@DESKTOP-4IAPGK2>
2017-10-16 15:17:02 +0300

    Add project files.

---
+++ Library(Angular_WebApi)/Controllers/UserAPIController.cs
@@ -0,0 +1,33 @@
+﻿using Library_Angular_WebApi_.DBContext;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Library_Angular_WebApi_.Controllers
+{
+    public class UserAPIController : BaseAPIController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(UserDB.TblUSERs.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody]TblUSER value)
+        {
+            UserDB.TblUSERs.Add(value);
+            return ToJson(UserDB.SaveChanges());
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]TblUSER value)
+        {
+            UserDB.Entry(value).State = EntityState.Modified;
+            return ToJson(UserDB.SaveChanges());
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            UserDB.TblUSERs.Remove(UserDB.TblUSERs.FirstOrDefault(x => x.Id == id));
+            return ToJson(UserDB.SaveChanges());
+        }
+    }
+}
---
+++ Library(Angular_WebApi)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Library_Angular_WebApi_.MvcApplication" Language="C#" %>
